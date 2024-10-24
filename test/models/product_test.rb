d37d99c33733d16c08cb1de964379062981ca037
Sangharsha Chaulagain <sangharsha.chaulagain@gmail.com>
2024-10-24 14:56:16 +1100

    add test for the product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,54 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_product(image_url)
+    Product.new(title: "My Book Title", description: "yyy", price: 1, image_url: image_url)
+  end
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title", description: "yyy", image_url: "zzz.jpg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+
+    product.price = 0.01
+  end
+
+  test "image_url" do
+    ok = %w[fred.gif fred.jpg FRED.JPG fred.png FRED.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
+
+    ok.each do |img_url|
+      assert new_product(img_url).valid?, "#{img_url} is valid."
+    end
+
+    bad.each do |img_url|
+      assert new_product(img_url).invalid?, "${img_url} is invalid."
+    end
+  end
+
+  test "product is not valid without the unique title" do
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 9.99, image_url: "lorem.jpg")
+    assert product.invalid?
+    assert_equal [I18n.translate("errors.messages.taken")], product.errors[:title]
+  end
 end
